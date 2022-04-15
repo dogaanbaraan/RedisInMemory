b984a553a7513aa81b454a8553f9a87bbf8b3f42
Baran Doğan <baran.dogan@mtteknoloji.com.tr>
2022-04-15 17:09:42 +0300

    IMemory cache ile bellekten data silme veya değiştirme

--- InMemoryApp.Web/Controllers/ProductController.cs
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -14,13 +14,36 @@ namespace InMemoryApp.Web.Controllers
         }
         public IActionResult Index()
         {
-            _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            //Bir key değerinin memoryde olup olmadığının tespit edilmesi
+            #region 1.Yol 
+            //if (String.IsNullOrEmpty(_memoryCache.Get<string>("zaman"))) //
+            //{
+            //    _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            //}
+            #endregion
+
+            #region 2.Yol
+            if (!_memoryCache.TryGetValue("zaman", out string zamanCache))
+            {
+                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            }
+
+            #endregion
+
             return View();
         }
 
         public IActionResult Show()
         {
-            ViewBag.zaman =  _memoryCache.Get<string>("zaman");
+            //_memoryCache.Remove("zaman"); //Memorycache üzerindeki seçmiş olduğumuz datayı cache üzerinde silmek için kullanılır.
+
+            _memoryCache.GetOrCreate<string>("zaman", entry => //Bu methodla beraber bu keye sahip değeri almaya çalışır
+                                                               //eğer memoryde bu değer yoksa memory üzerinde bu değeri oluşturur.
+            {
+                return DateTime.Now.ToString();
+            });
+
+            ViewBag.zaman = _memoryCache.Get<string>("zaman");
             return View();
         }
     }
