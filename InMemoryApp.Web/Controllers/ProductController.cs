cb969bec59e5e154bd9299281c62571c1ff487fd
Baran Doğan <baran.dogan@mtteknoloji.com.tr>
2022-04-15 15:50:05 +0300

    IMemoryCache ile get ve set metod kullanımı örneği

---
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+
+namespace InMemoryApp.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public ProductController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+        public IActionResult Index()
+        {
+            _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            ViewBag.zaman =  _memoryCache.Get<string>("zaman");
+            return View();
+        }
+    }
+}
