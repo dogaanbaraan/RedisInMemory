a4f83138517fbcef08e04357f90aef2c736228b7
Baran Doğan <baran.dogan@mtteknoloji.com.tr>
2022-04-18 11:34:54 +0300

    Absolute ve Sliding Expration, Priority yapısı, RegisterPostEvictionCallback yapısı projeye uygulandı.

--- InMemoryApp.Web/Controllers/ProductController.cs
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InMemoryApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 using System;
 
@@ -23,27 +24,47 @@ namespace InMemoryApp.Web.Controllers
             #endregion
 
             #region 2.Yol
-            if (!_memoryCache.TryGetValue("zaman", out string zamanCache))
+            //if (!_memoryCache.TryGetValue("zaman", out string zamanCache))
+            //{
+                MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+                options.AbsoluteExpiration = DateTime.Now.AddSeconds(10); /*verilen süre geçtikten sonra data cache üzerinden silinir.*/
+                /*options.SlidingExpiration = TimeSpan.FromSeconds(10);*/    //verilen süre tam olarak geçmeden o süre içerisinde
+                                                                         //veri istenirse verilen süre sürekli yenilenir.
+                options.Priority = CacheItemPriority.High;               //cachemiz dolduğunda silinecek olan datayı seçebilmek için datalara önem derecesi aktarılır.
+                                                                         //bu sayede önemli data dururken önemsiz data silinir.
+
+            options.RegisterPostEvictionCallback((key, value, reason, state) => //memoryden bir data silindiği zaman hangi sebeple silindiğini bildirmek için kullanılır.
             {
-                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
-            }
+                _memoryCache.Set("callback", $"{key} -> , {value} => sebep:{reason}");
+            });
+            _memoryCache.Set<string>("zaman", DateTime.Now.ToString(), options);
+
+            Product product = new Product { Id = 1, Name = "Kalem", Price = 200 };
+            _memoryCache.Set<Product>("product:1", product);
+            return View();
+            //}
 
             #endregion
 
-            return View();
         }
 
         public IActionResult Show()
         {
             //_memoryCache.Remove("zaman"); //Memorycache üzerindeki seçmiş olduğumuz datayı cache üzerinde silmek için kullanılır.
 
-            _memoryCache.GetOrCreate<string>("zaman", entry => //Bu methodla beraber bu keye sahip değeri almaya çalışır
-                                                               //eğer memoryde bu değer yoksa memory üzerinde bu değeri oluşturur.
-            {
-                return DateTime.Now.ToString();
-            });
+            //_memoryCache.GetOrCreate<string>("zaman", entry => //Bu methodla beraber bu keye sahip değeri almaya çalışır
+            //                                                   //eğer memoryde bu değer yoksa memory üzerinde bu değeri oluşturur.
+            //{
+            //    return DateTime.Now.ToString();
+            //});
 
-            ViewBag.zaman = _memoryCache.Get<string>("zaman");
+            _memoryCache.TryGetValue<string>("zaman", out string zamanCache);
+            _memoryCache.TryGetValue<string>("callback", out string callCache);
+            _memoryCache.TryGetValue<Product>("product:1", out Product productCache);
+
+            ViewBag.zaman = zamanCache;
+            ViewBag.callCache = callCache;
+            ViewBag.product = productCache;
             return View();
         }
     }
